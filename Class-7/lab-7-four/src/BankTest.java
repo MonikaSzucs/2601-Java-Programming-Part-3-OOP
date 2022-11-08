f7e1a9065c29617bdbb65e16b38cfd13817ced62
Monika Szucs <monika.szucs.work@gmail.com>
2022-11-08 14:20:23 -0800

    Lab 7 finished adding code still need to do docstring now

--- Class-7/lab-7-four/src/BankTest.java
+++ Class-7/lab-7-four/src/BankTest.java
@@ -1,14 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.awt.print.Book;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 class BankTest {
 
@@ -31,6 +28,15 @@ class BankTest {
 	void tearDown() throws Exception {
 	}
 
+	String getCustomerInfo(Customer customer){
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		PrintStream originalOut = System.out;
+		System.setOut(new PrintStream(outContent));
+		theEmptyBank.displayCustomerInformation(customer);
+		String output = (outContent.toString());
+		System.setOut(originalOut);
+		return output;
+	}
 	@Test
 	void test() {
 		//		  ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -41,37 +47,72 @@ class BankTest {
 		//        System.setOut(originalOut);
 
 		//Setup the empty test later
+		//display all customers
+		for(int i = 1; i<50;i++){
+			Customer temporaryCustomer = new Customer("FirstName"+i, "LastName"+i, "12345678"+i);
+			NullPointerException ex = assertThrows(NullPointerException.class, () -> {
+				theEmptyBank.addCustomer(temporaryCustomer);
+			});
 
-		Customer temporaryCustomer = new Customer("Justin", "Bieber", "12345678");
-		NullPointerException ex = assertThrows(NullPointerException.class, () -> {
+			double theBalance = 1000.0*i;
+			Account temporaryAccount = new Account("12345"+i,theBalance);
+
+			temporaryCustomer.setAccount(temporaryAccount);
+
 			theEmptyBank.addCustomer(temporaryCustomer);
-		});
-
-		Account temporaryAccount = new Account("12345",5000);
-
-//		for(int i = 0 ; i < 50 ; i++){
-//
-//		}
-		temporaryCustomer.setAccount(temporaryAccount);
-
-		theEmptyBank.addCustomer(temporaryCustomer);
-		theExpectedOutput+= "Customer [firstName=Justin, lastName=Bieber, passcode=12345678, account=Account " +
-				"[accountNumber=12345, balance=5000.0, active=true]]"+System.lineSeparator();
 
 
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+			theEmptyBank.displayCustomerInformation(temporaryCustomer);
+			String currentExpectedInfo = "Customer [firstName=FirstName"+i+", lastName=LastName"+i+", passcode=12345678"+i+", account=Account " +
+					"[accountNumber=12345"+i+", balance="+theBalance+", active=true]]"+System.lineSeparator();
 
-		PrintStream originalOut = System.out;
-		System.setOut(new PrintStream(outContent));
-		theEmptyBank.displayAllCustomers();
-		String output = (outContent.toString());
-		System.setOut(originalOut);
-		System.out.println(output);
-		System.out.println(theExpectedOutput);
+			assertEquals(getCustomerInfo(temporaryCustomer), currentExpectedInfo);
 
-		assertTrue(output.equals(theExpectedOutput));
+			theExpectedOutput+= "Customer [firstName=FirstName"+i+", lastName=LastName"+i+", passcode=12345678"+i+", account=Account " +
+					"[accountNumber=12345"+i+", balance="+theBalance+", active=true]]"+System.lineSeparator();
+
+			ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+			PrintStream originalOut = System.out;
+			System.setOut(new PrintStream(outContent));
+			theEmptyBank.displayAllCustomers();
+			String output = (outContent.toString());
+			System.setOut(originalOut);
+			String[] outputArray = output.split(System.lineSeparator());
+			String[] expectedArray = theExpectedOutput.split(System.lineSeparator());
+			Arrays.sort(outputArray);
+			Arrays.sort(expectedArray);
+
+			assertTrue(Arrays.equals(outputArray, expectedArray));
+		}
+
+		// close account
+		for(int i = 1; i<50;i++){
+			theEmptyBank.closeAccount("00");
+			theEmptyBank.closeAccount("12345"+i);
+			ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+			PrintStream originalOut = System.out;
+			System.setOut(new PrintStream(outContent));
+			theEmptyBank.displayAllCustomers();
+			String output = (outContent.toString());
+			System.setOut(originalOut);
+			if(!output.isEmpty()){
+				String[] outputArray = output.split(System.lineSeparator());
+//				System.out.println("the index is "+i+"outputArray is "+outputArray[0]);
+//				theEmptyBank.displayAllCustomers();
+				assertEquals(50-i-1,outputArray.length);
+			}
 
 
+			Customer temporaryCustomer = new Customer("FirstName"+i, "LastName"+i, "12345678"+i);
+			Customer inCorrectTemporaryCustomer = new Customer("Fake"+i, "LastName"+i, "12345678"+i);
+			assertEquals("Customer [firstName=Fake"+i+", lastName=LastName"+i+", passcode=12345678"+i+", account=null]"+System.lineSeparator(),getCustomerInfo(inCorrectTemporaryCustomer));
+//			assertEquals("",getCustomerInfo(temporaryCustomer));
+			assertEquals("Customer [firstName=FirstName"+i+", lastName=LastName"+i+", passcode=12345678"+i+", account=null]"+System.lineSeparator(),getCustomerInfo(temporaryCustomer));
+			//public static java.util.HashMap<java.lang.String,Customer> theBank
+
+
+
+		}
 //		System.out.println(theEmptyBank.toString());
 //		fail("Not yet implemented");
 
