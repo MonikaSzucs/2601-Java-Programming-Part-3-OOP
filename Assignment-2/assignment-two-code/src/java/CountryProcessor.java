7e8fb86f8202b0b96db57fef1bf3b361adc5a789
Monika Szucs <monika.szucs.work@gmail.com>
2022-12-04 15:53:29 -0800

    finished the streams and filters section

--- Assignment-2/assignment-two-code/src/java/CountryProcessor.java
+++ Assignment-2/assignment-two-code/src/java/CountryProcessor.java
@@ -52,6 +52,22 @@ class CountryProcessor {
         System.out.println();
         System.out.println("Printing the longest combination of country and capital name");
         printLongestCombination();
+
+        System.out.println();
+        System.out.println("Printing the number of letters in all country names put together");
+        printHowManyLettersInCountries();
+
+        System.out.println();
+        System.out.println("Printing capitals with a certain number of letters");
+        printCapitalsWithThisManyLetters(4, 5);
+
+        System.out.println();
+        System.out.println("Printing all countries that do not end with a certain letter");
+        printAllCountriesThatDoNotEndWith('n');
+
+        System.out.println();
+        System.out.println("Printing all capitals that contain the letter into a single string with no space");
+        printAllCapitalsThatContainLetterIntoASingleStringNoSpaces('z');
     }
 
    private static void printLongestCapitalCity() {
@@ -75,7 +91,40 @@ class CountryProcessor {
     }
 
     private static void printLongestCombination() {
+        Optional<CountryCapital>longestCombination = allCountryCapital.stream()
+                .filter(n -> !n.getCapital().isBlank())
+                .max(Comparator.comparing(n -> n.getCountry().length() + n.getCapital().length()));
+        longestCombination.ifPresent(n -> System.out.println("Longest combination of country and capital is " + n.getCountry() + ", " + n.getCapital()));
+    }
 
+    private static void printHowManyLettersInCountries() {
+        long countAllLength = allCountryCapital.stream()
+                .mapToInt(n -> n.getCountry().length()).sum();
+        System.out.println(countAllLength);
+    }
+
+    private static void printCapitalsWithThisManyLetters(final int min, final int max) {
+        List<CountryCapital> capitalLetterCountBetween = allCountryCapital.stream()
+                .filter(n -> n.getCapital().length() >= min && n.getCapital().length() <= max)
+                .collect(Collectors.toList());
+        capitalLetterCountBetween.forEach(System.out::println);
+    }
+
+    private static void printAllCountriesThatDoNotEndWith(final char letter) {
+        List<CountryCapital> countriesThatDoNotEndWithLetter = allCountryCapital.stream()
+                .filter(n -> !n.getCountry().isBlank() && !n.getCountry().endsWith(String.valueOf(letter)))
+                .collect(Collectors.toList());
+        countriesThatDoNotEndWithLetter.forEach(System.out::println);
+    }
+
+    private static void printAllCapitalsThatContainLetterIntoASingleStringNoSpaces(final char letter) {
+        String countriesThatContain = allCountryCapital.stream()
+                .filter(n -> !n.getCapital().isBlank() && n.getCapital().toLowerCase().contains(String.valueOf(letter)))
+                .map(n -> n.getCapital())
+                .collect(Collectors.joining(""));
+        //countriesThatContain.forEach(System.out::println);
+
+        System.out.println(countriesThatContain);
     }
 
    private static CountryCapital parseCountryCapital(final String commaSeparatedInfo) {
